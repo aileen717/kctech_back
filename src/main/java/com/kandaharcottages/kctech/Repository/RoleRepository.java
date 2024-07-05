49b9a7c5e9a56caa3665ec085f4dec85b2041c60
aileen717 <tenorioaileen07@gmail.com>
2024-07-05 13:00:23 +0800

    kulang ng config, dto.loginrequest at service

---
+++ src/main/java/com/kandaharcottages/kctech/Repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.kandaharcottages.kctech.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kandaharcottages.kctech.Model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
