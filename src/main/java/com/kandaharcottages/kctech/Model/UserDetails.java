faf2e49c326f6c29c7c8ea0c58ecf97c14e61502
aileen717 <tenorioaileen07@email.com>
2024-07-12 12:52:39 +0800

    kulang pa ng config, dto, at service

--- src/main/java/com/kandaharcottages/kctech/Model/UserDetails.java
+++ src/main/java/com/kandaharcottages/kctech/Model/UserDetails.java
@@ -1,6 +1,8 @@
 package com.kandaharcottages.kctech.Model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
